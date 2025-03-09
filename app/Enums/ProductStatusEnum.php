50536954b5939657cae98713d48bafef691b0b23
Idada Osaigbovo James <wijojames@gmail.com>
2025-03-09 16:50:15 +0100

    feat: created enum for available and unavailabe products

---
+++ app/Enums/ProductStatusEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+use Spatie\Enum\Laravel\Enum;
+
+/**
+ * @method static self AVAILABLE()
+ * @method static self UNAVAILABLE()
+ */
+final class ProductStatusEnum extends Enum
+{
+}
