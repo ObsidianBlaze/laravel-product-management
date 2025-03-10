5f00aa1626e938af7c476407bd864cb9e4e1ccf4
Idada Osaigbovo James <wijojames@gmail.com>
2025-03-10 16:27:45 +0100

    feat: added bodyParameters method to manage how docs display the body of a request

--- app/Http/Requests/StoreProductRequest.php
+++ app/Http/Requests/StoreProductRequest.php
@@ -4,10 +4,12 @@ namespace App\Http\Requests;
 
 use App\Enums\ProductStatusEnum;
 use Illuminate\Foundation\Http\FormRequest;
+use Knuckles\Scribe\Extracting\ParamHelpers;
 use Spatie\Enum\Laravel\Rules\EnumRule;
 
 class StoreProductRequest extends FormRequest
 {
+    use ParamHelpers;
     public function authorize(): bool
     {
         return true;
@@ -19,6 +21,29 @@ class StoreProductRequest extends FormRequest
             'name' => 'required|string|max:255',
             'category' => 'required|string|max:255',
             'status' => ['required', new EnumRule(ProductStatusEnum::class)],
+        ];
+    }
+
+    /**
+     * Define the body parameters for Scribe.
+     *
+     * @return array
+     */
+    public function bodyParameters(): array
+    {
+        return [
+            'name' => [
+                'description' => 'The name of the product.',
+                'example' => 'Samsung',
+            ],
+            'category' => [
+                'description' => 'The product category.',
+                'example' => 'Electronics',
+            ],
+            'status' => [
+                'description' => 'The product status (available/unavailable).',
+                'example' => 'available',
+            ],
         ];
     }
 }
