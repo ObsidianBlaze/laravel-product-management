ef84fa00447f530fe6de35a978a68b367307c26e
Idada Osaigbovo James <wijojames@gmail.com>
2025-03-11 01:20:05 +0100

    refactor: updating filament to allow only admin visit dashboard

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,9 +52,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessFilament(): bool
     {
-        return $this->is_admin;
-//        return $this->is_admin && $this->hasVerifiedEmail();
-
-//        return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
+        return $this->isAdmin();
     }
 }
